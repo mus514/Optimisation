d16cfb60c798b7d4136e58de6ca95f5559a15cf6
mustapha bouhsen <mutaphabouhsen@MacBook-Air-de-mustapha.local>
2021-07-05 01:02:04 -0400

    first

---
+++ source.R
@@ -0,0 +1,22 @@
+importData <- function(x)
+{
+  if(x == "arf")
+  {
+    
+  }
+  
+  if(x == "sf")
+  {
+  
+  }
+  
+  if(x == "sbc-vc")
+  {
+    
+  }
+  
+  if(x == "sbc-vc")
+  {
+    
+  }
+}
