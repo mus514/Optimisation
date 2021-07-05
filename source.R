bec6bed19ad6a8b1675ca14d8cb5505a50fb1cb6
mustapha bouhsen <mutaphabouhsen@MacBook-Air-de-mustapha.local>
2021-07-05 04:02:54 -0400

    importData

--- source.R
+++ source.R
@@ -10,16 +10,37 @@ importData <- function(x)
   if(grepl("vc+", x))
   {
     data <- read_excel("SBC - VC.xlsx", sheet = paste0("vi_" , x))
-  }
+  }                 
   
   else
   {
     data <- read_excel("SBC.xlsx", sheet = paste0("vi_", x))
   }
   
+  data <- data[2:2561, c(1, 2, 4, 6, 7, 8, 9, 16, 17, 18, 19, 
+                   20, 21, 28, 29, 30, 31, 32, 33, 40, 41,
+                   42, 43, 44, 45, 52, 53)]
+  
+  names(data)[1:3] <- c("H", "Inventory", "n")
+  
+  name <- c("UB", "LB", "Gap", "Time", "Opt", "Unsolved")
+  
+  k <- 2
+  
+  for(i in c(4, 10, 16, 22))
+  {
+    for(j in 0:5)
+    {
+      names(data)[i+j] <- paste0("V", k,"_",name[j+1])
+    }
+    
+    k <- k + 1
+    
+  }
+  
   data
- 
+  
 }
 
-y<- importData("cus")
+y<- importData("cos")
 
