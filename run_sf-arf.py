5d25d45292d5b69b8ac83e0eb7106a5accb0ecb4
mustapha bouhsen <mustapha@MacBook-Air-de-mustapha.local>
2021-08-02 02:52:50 -0400

    add run sf

--- run_sf-arf.py
+++ run_sf-arf.py
@@ -60,31 +60,71 @@ def copy_missing(file, sheet):
                                 os.mkdir(f"irp_lp_solver-master/input/missing/{i}/V{j}/{k}")
 
     except:
-        print("Folder Already exist")
+        print("Folder Already exist, delete all the folders and restart !!!")
+
+    for i in ordering:
+        if(i == 0):
+            for j in data[data["ordering"] == 0]["vehicles"].unique():
+                temp = data[data["ordering"] == 0]
+                temp = temp[temp["vehicles"] == j]
+                temp = list(temp["name"])
+
+                if len(temp) <= 8:
+                    for k in temp:
+                        shutil.copy(f"irp_lp_solver-master/input/Instances/Original/{k}",
+                                    f"irp_lp_solver-master/input/missing/Original/V{j}/0")
+
+                temp_1 = []
+
+                if len(temp) > 8:
+                    l = int((len(temp) - len(temp) % 8) / 8)
+
+                    if (len(temp) % 8 == 0):
+                        for k in range(0, l*8, 8):
+                            temp_1.append(temp[k:k+8])
+
+                    else:
+                        for k in range(0, l*8, 8):
+                            temp_1.append(temp[k:k+8])
+                        temp_1.append(temp[l*8:len(temp)])
+
+                    for k in range(len(temp_1)):
+                        for n in temp_1[k]:
+                            shutil.copy(f"irp_lp_solver-master/input/Instances/Original/{n}",
+                                        f"irp_lp_solver-master/input/missing/Original/V{j}/{k}")
+
+        else:
+            for j in data[data["ordering"] == i]["vehicles"].unique():
+                temp = data[data["ordering"] == i]
+                temp = temp[temp["vehicles"] == j]
+                temp = list(temp["name"])
+
+                if len(temp) <= 8:
+                    for k in temp:
+                        shutil.copy(f"irp_lp_solver-master/input/Instances/{i}/{k}",
+                                    f"irp_lp_solver-master/input/missing/{i}/V{j}/0")
+
+                temp_1 = []
+
+                if len(temp) > 8:
+                    l = int((len(temp) - len(temp) % 8) / 8)
+
+                    if (len(temp) % 8 == 0):
+                        for k in range(0, l*8, 8):
+                            temp_1.append(temp[k:k+8])
+
+                    else:
+                        for k in range(0, l*8, 8):
+                            temp_1.append(temp[k:k+8])
+                        temp_1.append(temp[l*8:len(temp)])
+
+                    for k in range(len(temp_1)):
+                        for n in temp_1[k]:
+                            shutil.copy(f"irp_lp_solver-master/input/Instances/{i}/{n}",
+                                        f"irp_lp_solver-master/input/missing/{i}/V{j}/{k}")
+
+
+
 
     return (data)
 
-
-
-
-
-
-
-
-
-
-
-data = copy_missing("ARF_SF-missing" ,"SF")
-
-data = data[data["ordering"] == 1]
-data = data[data["vehicles"] == 5]
-
-print(len(data))
-
-# x = [[[i for i in range(2)] for j in range(5)] for k in range(15)]
-
-
-
-#print(y)
-# os.mkdir("irp_lp_solver-master/input/Instances/Original/allo")
-#shutil.copy("test.txt", "allo")
