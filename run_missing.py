8fb201df50ea33130115fd492a7a56ec0db4db7a
Mustapha Bouhsen <mustapha.bouhsen.1@ulaval.ca>
2021-08-27 01:57:46 -0400

    update

--- run_missing.py
+++ run_missing.py
@@ -6,10 +6,6 @@ import shutil, os
 
 
 def make_cfg(sheet, folder, vehicles, ordering):
-    """
-    :return: cfg scripte
-    """
-
     scripte = f"""################################################################################
 #
 # Classic IRP solver: example of the input configuration file.
@@ -60,12 +56,13 @@ sec_strategy = 1
     return scripte
 
 
+##
+###
+##
+
 def make_sh(sheet, ordering, time, end, default="SLURM_ARRAY_TASK_ID"):
-    """
-    :return: Sh scripte
-    """
     scripte = f"""
-#!/bin/bash
+    #!/bin/bash
 #SBATCH --account=def-mardar
 #SBATCH --mail-user=bouhsen.m@gmail.com
 #SBATCH --mail-type=FAIL
@@ -84,10 +81,7 @@ echo "Starting task $SLURM_ARRAY_TASK_ID"
     return scripte
 
 
-def run_scripte(vector, sheet):
-    """
-    :return: run scripte
-    """
+def run_sh(vector, sheet):
     file = open("irp_lp_solver-master/run.sh", 'w')
     file.write("#!/bin/bash \n \n \n")
     for i in vector:
@@ -100,15 +94,14 @@ def run_scripte(vector, sheet):
 # in each part we copy maximun 8 instances,
 ##
 
-def make_missing(file, sheet):
-    # Import data
-    data = pd.read_excel(file + ".xlsx", sheet_name=sheet)
-
-    # Sort by all the orderings
-    ordering = data["ordering"].unique()
+def run_calculs(file, sheet):
 
     try:
-        # Creating the folders for the input, output, cfg and sh
+        ## Creat different folders and divied them in many part depending on number of missing instances
+        # in each part we copy maximun 8 instances.
+
+        data = pd.read_excel(file + ".xlsx", sheet_name=sheet)
+        ordering = data["ordering"].unique()
         os.mkdir(f"irp_lp_solver-master/input/missing_{sheet}")
         os.mkdir(f"irp_lp_solver-master/output/missing_{sheet}")
         os.mkdir(f"irp_lp_solver-master/cfg_{sheet}")
@@ -117,15 +110,10 @@ def make_missing(file, sheet):
         end = []
 
         for i in ordering:
-
-            # case if the ordering is "Original"
             if i == 0:
-                # create empty folder for input and output
                 os.mkdir(f"irp_lp_solver-master/input/missing_{sheet}/Original")
                 os.mkdir(f"irp_lp_solver-master/output/missing_{sheet}/Original")
-
                 for j in data[data["ordering"] == 0]["vehicles"].unique():
-                    # field the empty Original folder by the missing vehicles
                     os.mkdir(f"irp_lp_solver-master/input/missing_{sheet}/Original/V{j}")
                     os.mkdir(f"irp_lp_solver-master/output/missing_{sheet}/Original/V{j}")
 
@@ -167,7 +155,7 @@ def make_missing(file, sheet):
                                 file_name.close()
                                 counter += 1
 
-            # Other ordering from 1 to 15. Same logic as Original case
+
             else:
                 os.mkdir(f"irp_lp_solver-master/input/missing_{sheet}/{i}")
                 os.mkdir(f"irp_lp_solver-master/output/missing_{sheet}/{i}")
@@ -213,112 +201,108 @@ def make_missing(file, sheet):
             end.append(counter - 1)
             counter = 1
 
-            # Coping and dividing the instances. We took the instance from the Instance in input and we copy them to the
-            # specific folder we already create in the previews code
+        # Coping and dividing the instances. We took the instance from the Instance in input and we copy them to the
+        # specific folder we already create in the previews code
 
-            time = []  # List of the times we need to run in CalculCanada
-            counter = 0;
+        time = []
+        counter = 0;
+        for i in ordering:
+            if (i == 0):
+                for j in data[data["ordering"] == 0]["vehicles"].unique():
+                    temp = data[data["ordering"] == 0]
+                    temp = temp[temp["vehicles"] == j]
+                    temp = list(temp["name"])
 
-            for i in ordering:
-                # case if the ordering is "Original"
-                if (i == 0):
+                    if len(temp) <= 8:
+                        for k in temp:
+                            shutil.copy(f"irp_lp_solver-master/input/Instances/Original/{k}",
+                                        f"irp_lp_solver-master/input/missing_{sheet}/Original/V{j}/0")
+                            counter += 1
 
-                    for j in data[data["ordering"] == 0]["vehicles"].unique():
-                        temp = data[data["ordering"] == 0]
-                        temp = temp[temp["vehicles"] == j]
-                        temp = list(temp["name"])
+                    temp_1 = []
 
-                        # Same logic as the previews code, We copy the instances by 8 or less
-                        if len(temp) <= 8:
-                            for k in temp:
-                                shutil.copy(f"irp_lp_solver-master/input/Instances/Original/{k}",
-                                            f"irp_lp_solver-master/input/missing_{sheet}/Original/V{j}/0")
+                    if len(temp) > 8:
+                        l = int((len(temp) - len(temp) % 8) / 8)
+
+                        if len(temp) % 8 == 0:
+                            for k in range(0, l * 8, 8):
+                                temp_1.append(temp[k:k + 8])
+
+                        else:
+                            for k in range(0, l * 8, 8):
+                                temp_1.append(temp[k:k + 8])
+                            temp_1.append(temp[l * 8:len(temp)])
+
+                        for k in range(len(temp_1)):
+                            for n in temp_1[k]:
+                                shutil.copy(f"irp_lp_solver-master/input/Instances/Original/{n}",
+                                            f"irp_lp_solver-master/input/missing_{sheet}/Original/V{j}/{k}")
                                 counter += 1
 
-                        temp_1 = []  # List of the names of the missing instances
+            else:
+                for j in data[data["ordering"] == i]["vehicles"].unique():
+                    temp = data[data["ordering"] == i]
+                    temp = temp[temp["vehicles"] == j]
+                    temp = list(temp["name"])
 
-                        if len(temp) > 8:
-                            l = int((len(temp) - len(temp) % 8) / 8)
+                    if len(temp) <= 8:
+                        for k in temp:
+                            shutil.copy(f"irp_lp_solver-master/input/Instances/{i}/{k}",
+                                        f"irp_lp_solver-master/input/missing_{sheet}/{i}/V{j}/0")
+                            counter += 1
 
-                            if len(temp) % 8 == 0:
-                                for k in range(0, l * 8, 8):
-                                    temp_1.append(temp[k:k + 8])
+                    temp_1 = []
 
-                            else:
-                                for k in range(0, l * 8, 8):
-                                    temp_1.append(temp[k:k + 8])
-                                temp_1.append(temp[l * 8:len(temp)])
+                    if len(temp) > 8:
+                        l = int((len(temp) - len(temp) % 8) / 8)
 
-                            for k in range(len(temp_1)):
-                                for n in temp_1[k]:
-                                    shutil.copy(f"irp_lp_solver-master/input/Instances/Original/{n}",
-                                                f"irp_lp_solver-master/input/missing_{sheet}/Original/V{j}/{k}")
-                                    counter += 1
+                        if len(temp) % 8 == 0:
+                            for k in range(0, l * 8, 8):
+                                temp_1.append(temp[k:k + 8])
 
-                # Other ordering from 1 to 15. Same logic as Original case
-                else:
-                    for j in data[data["ordering"] == i]["vehicles"].unique():
-                        temp = data[data["ordering"] == i]
-                        temp = temp[temp["vehicles"] == j]
-                        temp = list(temp["name"])
+                        else:
+                            for k in range(0, l * 8, 8):
+                                temp_1.append(temp[k:k + 8])
+                            temp_1.append(temp[l * 8:len(temp)])
 
-                        if len(temp) <= 8:
-                            for k in temp:
-                                shutil.copy(f"irp_lp_solver-master/input/Instances/{i}/{k}",
-                                            f"irp_lp_solver-master/input/missing_{sheet}/{i}/V{j}/0")
+                        for k in range(len(temp_1)):
+                            for n in temp_1[k]:
+                                shutil.copy(f"irp_lp_solver-master/input/Instances/{i}/{n}",
+                                            f"irp_lp_solver-master/input/missing_{sheet}/{i}/V{j}/{k}")
                                 counter += 1
-
-                        temp_1 = []
-
-                        if len(temp) > 8:
-                            l = int((len(temp) - len(temp) % 8) / 8)
-
-                            if len(temp) % 8 == 0:
-                                for k in range(0, l * 8, 8):
-                                    temp_1.append(temp[k:k + 8])
-
-                            else:
-                                for k in range(0, l * 8, 8):
-                                    temp_1.append(temp[k:k + 8])
-                                temp_1.append(temp[l * 8:len(temp)])
-
-                            for k in range(len(temp_1)):
-                                for n in temp_1[k]:
-                                    shutil.copy(f"irp_lp_solver-master/input/Instances/{i}/{n}",
-                                                f"irp_lp_solver-master/input/missing_{sheet}/{i}/V{j}/{k}")
-                                    counter += 1
-                time.append(counter)
-                counter = 0
-
-            # create .sh file
+            time.append(counter)
             counter = 0
-            file = []
-            for i in ordering:
-                if (i == 0):
-                    file_name = f"irp_lp_solver-master/sh_{sheet}/{sheet}-Original-1-{end[counter]}.sh"
-                    file.append(f"{sheet}-Original-1-{end[counter]}.sh")
-                    file_name = open(file_name, 'a')
-                    file_name.write(make_sh(sheet, "Original", time[counter], end[counter]))
-                    file_name.close()
 
-                else:
-                    file_name = f"irp_lp_solver-master/sh_{sheet}/{sheet}-{i}-1-{end[counter]}.sh"
-                    file.append(f"{sheet}-{i}-1-{end[counter]}.sh")
-                    file_name = open(file_name, 'a')
-                    file_name.write(make_sh(sheet, i, time[counter], end[counter]))
-                    file_name.close()
+        # create .sh file
 
-                counter += 1
+        counter = 0
+        file = []
+        for i in ordering:
+            if (i == 0):
+                file_name = f"irp_lp_solver-master/sh_{sheet}/{sheet}-Original-1-{end[counter]}.sh"
+                file.append(f"{sheet}-Original-1-{end[counter]}.sh")
+                file_name = open(file_name, 'a')
+                file_name.write(make_sh(sheet, "Original", time[counter], end[counter]))
+                file_name.close()
 
-            # Create the run scipte
-            run_scripte(file, sheet)
+            else:
+                file_name = f"irp_lp_solver-master/sh_{sheet}/{sheet}-{i}-1-{end[counter]}.sh"
+                file.append(f"{sheet}-{i}-1-{end[counter]}.sh")
+                file_name = open(file_name, 'a')
+                file_name.write(make_sh(sheet, i, time[counter], end[counter]))
+                file_name.close()
 
+            counter += 1
+
+        run_sh(file, sheet)
+
+        print("See the folder")
 
     except:
-        print("Folder Already exist, delete all the folders and restart !!!")
+        print("Delete every thing created and repeat")
 
-    print("End")
+
+
 
 if __name__ == "__main__":
-    make_missing("ARF_SF-missing", "SF")
-#    #copy_missing("SBC-missing", "cos")
+    run_calculs("ARF_SF-missing", "SF")
